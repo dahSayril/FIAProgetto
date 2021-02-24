f7c6d5ebea0d4e26d9fbe569cd44defdbf3bd43e
Antonio Cacciapuoti <yantcaccia@gmail.com>
2021-02-24 16:16:53 +0100

    pca completed
    skrt

--- src/pca.js
+++ src/pca.js
@@ -4,24 +4,60 @@ require('data-forge-fs');
 
 function pcaProcess(){
 
-    let dataFrame = dataForge.readFileSync('../datasource/SpotifyFeatures.csv').parseCSV();
-    let newDataFrame = dataFrame.subset(["acousticness", "danceability", "energy", "instrumentalness", "liveness", "speechiness", "valence"]);
-    
-    let aNewDataFrame = newDataFrame.between(0, 2000);
-    
-    let data = aNewDataFrame.toRows();
+    // Carico il DataSet
+    let dataFrame = dataForge.readFileSync('../datasource/SpotifyCSV.csv').parseCSV();
 
-    console.log("Ok done");
+    // Seleziono solo le features che mi interessano
+    let newDataFrame = dataFrame.subset(["Acousticness", "Danceability", "Energy", "Instrumentalness", "Liveness", "Speechiness", "Valence", "Popularity"]);
+    
+    // DataSet come array di array
+    let data = newDataFrame.toRows();
 
+    // Numero di elementi (canzoni) nel DataSet
+    let dimension = data.length;
+
+    // Calcolo autovettori e autovalori per PCA
     var vectors = pca.getEigenVectors(data);
 
-    console.log(vectors);
-
+    // Percentuale di accuratezza considerando il primo, i primi due e i primi tre autovettori
     console.log("Percentuali: ");
-
     console.log("Primo: " + pca.computePercentageExplained(vectors,vectors[0]));
     console.log("Primi due: " + pca.computePercentageExplained(vectors,vectors[0], vectors[1]));
     console.log("Primi tre: " + pca.computePercentageExplained(vectors,vectors[0], vectors[1], vectors[2]));
+
+    // DataSet trasformato sulle prime tre componenti principali individuate
+    let adData = pca.computeAdjustedData(data,vectors[0], vectors[1], vectors[2]).adjustedData; // N.B. Array di array
+
+    // Conterra' ogni canzone con un punteggio shiftato di +50
+    // In questo modo il nuovo DataSet conterra' valori compresi tra 0 e 100 come l'originale
+    let adjustedData = [];
+
+    // Shifto punteggio di ogni elemento a +50
+    adData.forEach(element => {
+        let newElement = element.map(score=>Math.round(score+50));
+        adjustedData.push(newElement);
+    });
+
+    // Nuovo DataSource che conterra' il DataSet trasformato sulle PC individuate
+    let myNewDataSource = [];
+
+    // Riempo il nuovo DataSource
+    for (i = 0; i < dimension; i++){
+
+        let mySong = {};
+        mySong.PC1 = adjustedData[0][i];
+        mySong.PC2 = adjustedData[1][i];
+        mySong.PC3 = adjustedData[2][i];
+
+        myNewDataSource.push(mySong);
+
+    }
+
+    // Creo nuovo DataFrame dal DataSource e poi lo esporto
+    let newDataFrame2 = new dataForge.DataFrame({
+		values: myNewDataSource
+	});
+    newDataFrame2.asCSV().writeFileSync('../datasource/datasetComponentiPrincipali.csv');
 
 }
 
