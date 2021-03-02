c604a026eecdbf87d713582018de3b69af320046
Antonio Cacciapuoti <yantcaccia@gmail.com>
2021-03-02 11:01:17 +0100

    standardize

--- src/pca.js
+++ src/pca.js
@@ -8,10 +8,24 @@ function pcaProcess(){
     let dataFrame = dataForge.readFileSync('../datasource/SpotifyCSV.csv').parseCSV();
 
     // Seleziono solo le features che mi interessano
-    let newDataFrame = dataFrame.subset(["Acousticness", "Danceability", "Energy", "Valence","Beats Per Minute (BPM)"]);
+    let features = ["Acousticness", "Danceability", "Energy", "Valence","Beats Per Minute (BPM)"]
+    let subDataFrame = dataFrame.subset(features);
+
+    var columns = subDataFrame.getColumns();
+    var arrayOfColumns = columns.toArray();
     
+    let myColumns = []; // Array di array (ogni array e' una colonna)
+
+    for (var column in arrayOfColumns) {
+        let myColumn = [];
+        myColumn.push(...arrayOfColumns[column].series.content.values);
+        standardize(myColumn);
+        myColumns.push(myColumn);
+    }
+
+
     // DataSet come array di array
-    let data = newDataFrame.toRows();
+    let data = subDataFrame.toRows();
 
     // Numero di elementi (canzoni) nel DataSet
     let dimension = data.length;
@@ -24,6 +38,7 @@ function pcaProcess(){
     console.log("Primo: " + pca.computePercentageExplained(vectors,vectors[0]));
     console.log("Primi due: " + pca.computePercentageExplained(vectors,vectors[0], vectors[1]));
     console.log("Primi tre: " + pca.computePercentageExplained(vectors,vectors[0], vectors[1], vectors[2]));
+    
     // DataSet trasformato sulle prime tre componenti principali individuate
     let adData = pca.computeAdjustedData(data, vectors[0], vectors[1], vectors[2]).adjustedData; // N.B. Array di array
 
@@ -61,6 +76,41 @@ function pcaProcess(){
 		values: myNewDataSource
 	});
     newDataFrame2.asCSV().writeFileSync('../datasource/datasetComponentiPrincipali.csv');
+}
+
+function getStandardDeviation(numbersArr, meanVal) {
+    
+    var SDprep = 0;
+    for(var key in numbersArr) {
+        if(numbersArr[key]!=""||numbersArr[key]){
+            SDprep += Math.pow((parseFloat(numbersArr[key]) - meanVal),2);
+        }
+    }
+    var SDresult = Math.sqrt(SDprep/numbersArr.length);
+    return SDresult;
+
+}
+
+function getMean(array){
+    let n = array.length;
+    let sum = 0;
+    array.forEach((value, index, array)=>{
+        if(value!=""||value){
+            sum += parseInt(value);
+        } else {
+            n--;
+        }
+    });
+    return sum/n;
+}
+
+function standardize(array){
+    const mean = getMean(array);
+    const standardDeviation = getStandardDeviation(array, mean);
+    array.forEach((value, index, array)=>{
+        zscore = (value - mean)/standardDeviation;
+        array[index] = zscore;
+    });
 }
 
 pcaProcess();
